7d348786468446ea598a97cdabda791a0650fb30
kwonja <42410000+kwonja@users.noreply.github.com>
2021-04-29 14:44:59 +0900

    샘플

---
+++ sample3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="sample3" tilewidth="16" tileheight="16" tilecount="5115" columns="93">
+ <image source="DVwZGR.png" width="1498" height="888"/>
+</tileset>
