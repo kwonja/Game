5993483c70de1c499c7782a599e57d29ed478560
the-groot <sangwoo0795@naver.com>
2021-05-27 23:57:55 +0900

    화살표 추가

---
+++ sangwoo/maps2/Pointer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Pointer" tilewidth="16" tileheight="16" tilecount="40" columns="20">
+ <image source="Pointer.png" width="320" height="32"/>
+</tileset>
